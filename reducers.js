f95bf56044dcba121d41b02032187820a7ca1dff
aqilusman45 <aqilusman45@gmail.com>
2020-04-02 07:46:17 +0500

    feat: switch cases based reducers for goals and todos

---
+++ reducers.js
@@ -0,0 +1,33 @@
+exports.todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.id);
+    case "UPDATE_TODO":
+      return state.map(todo =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+};
+
+exports.goals = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
+    case "UPDATE_GOAL":
+      return state.map(goal =>
+        goal.id !== action.id
+          ? goal
+          : Object.assign({}, goal, { complete: !action.complete })
+      );
+    default:
+      return state;
+  }
+};
